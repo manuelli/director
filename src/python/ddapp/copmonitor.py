25403ca96b86821c7d5dbd410bc9426ec964c72e
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-08-20 20:26:42 -0400

    add interface to edgeDistance computation

--- src/python/ddapp/copmonitor.py
+++ src/python/ddapp/copmonitor.py
@@ -4,6 +4,7 @@ import ddapp.objectmodel as om
 from ddapp import lcmUtils
 from ddapp import applogic as app
 from ddapp.utime import getUtime
+from ddapp import vtkNumpy as vnp
 from ddapp import visualization as vis
 from ddapp.debugVis import DebugData
 from scipy.spatial import Delaunay
@@ -14,11 +15,12 @@ from time import time
 from time import sleep
 
 class COPMonitor(object):
-    LONG_FOOT_CONTACT_POINTS = [[-0.13, 0.0562, 0.0],
-                                [-0.13, -0.0562, 0.0], 
-                                [0.17, 0.0562, 0.0],
-                                [0.17, -0.0562, 0.0]]
+    LONG_FOOT_CONTACT_POINTS = [[0.17, 0.0562, 0.0],
+                                [0.17, -0.0562, 0.0],
+                                [-0.13, -0.0562, 0.0],
+                                [-0.13, 0.0562, 0.0]]
     DESIRED_INTERIOR_DISTANCE = 0.05
+    printDebugData = True
 
     def __init__(self, robotSystem, view):
 
@@ -41,16 +43,16 @@ class COPMonitor(object):
         om.findObjectByName('measured cop').setProperty('Visible', False)
 
         vis.updatePolyData(d.getPolyData(), 'measured cop right', view=self.view, parent='robot state model')
-        om.findObjectByName('measured cop right').setProperty('Visible', False)
+        om.findObjectByName('measured cop right').setProperty('Visible', True)
 
         vis.updatePolyData(d.getPolyData(), 'measured cop left', view=self.view, parent='robot state model')
-        om.findObjectByName('measured cop left').setProperty('Visible', False)
+        om.findObjectByName('measured cop left').setProperty('Visible', True)
 
         
         self.robotStateModel.connectModelChanged(self.update)
 
     def update(self, unused):
-        if (om.findObjectByName('measured cop').getProperty('Visible') and self.robotStateJointController.lastRobotStateMessage or om.findObjectByName('measured cop right').getProperty('Visible') or om.findObjectByName('measured cop left').getProperty('Visible')):
+        if ((om.findObjectByName('measured cop').getProperty('Visible') or om.findObjectByName('measured cop right').getProperty('Visible') or om.findObjectByName('measured cop left').getProperty('Visible')) and self.robotStateJointController.lastRobotStateMessage):
 
             if self.dialogVisible == False:
                 self.warningButton.setVisible(True)
@@ -120,19 +122,76 @@ class COPMonitor(object):
 
                 # handles the right and left cop monitors
                 redColorStatus = [1,0,0]
+                greenColorStatus = [0,1,0]
+                edgeDetectionThreshold = 0.03
+
+                # print 'left foot points'
+                # print lFootContacts
+                # print type(lFootContacts)
+
+                def computeColorStatus(dist, threshold):
+                    # alpha = min(max(0,dist), threshold)/threshold
+                    alpha = 1;
+                    if dist < edgeDetectionThreshold:
+                        alpha = 0
+
+
+                    colorStatus = [(1-alpha), alpha, 0]
+                    return colorStatus
+
+                worldToRFoot = rFootTransform.GetLinearInverse();
+                numpyFootContacts = np.array(self.LONG_FOOT_CONTACT_POINTS)
+                numpyFootContacts = numpyFootContacts[:,0:2];
+                num_pts = 4
+                contacts = numpyFootContacts.reshape(2*num_pts,1)
 
                 if self.rightInContact:
+                    num_pts = 4
+                    cop_right_foot_frame = worldToRFoot.TransformPoint(measured_cop_right[0:3])
+                    dist = self.ddDrakeWrapper.drakeSignedDistanceInsideConvexHull(num_pts,contacts, cop_right_foot_frame[0:2])
+
+                    # print "measure COP in foot frame is "
+                    # print cop_right_foot_frame
+
+                    edgeDist = self.ddDrakeWrapper.drakeDistanceToEdges(num_pts, contacts, cop_right_foot_frame[0:2])
+
+                    if self.printDebugData:
+                        print 'right foot distance to convex hull'
+                        print dist
+
+                        print 'min distance edge left/right'
+                        print min(edgeDist[1], edgeDist[3])
+
+                        print "min edge distance front/back"
+                        print min(edgeDist[0], edgeDist[2])
+
+
+                    colorStatus = computeColorStatus(dist, edgeDetectionThreshold)
+                    # colorStatus = redColorStatus
+
                     d = DebugData()
                     d.addSphere(measured_cop_right[0:3], radius=0.02)
-                    vis.updatePolyData(d.getPolyData(), 'measured cop right', view=self.view, parent='robot state model').setProperty('Color', redColorStatus)
+                    vis.updatePolyData(d.getPolyData(), 'measured cop right', view=self.view, parent='robot state model').setProperty('Color', colorStatus)
                 else:
                     d = DebugData()
                     vis.updatePolyData(d.getPolyData(), 'measured cop right', view=self.view, parent='robot state model')
 
                 if self.leftInContact:
+                    num_pts = 4
+                    contacts = lFootContacts[:, 0:2]
+                    contacts = contacts.reshape(2*num_pts,1)
+                    dist = self.ddDrakeWrapper.drakeSignedDistanceInsideConvexHull(num_pts,contacts, measured_cop_left[0:2])
+
+                    if self.printDebugData:
+                        print 'left foot distance to foot edge'
+                        print dist
+
+                    colorStatus = computeColorStatus(dist, edgeDetectionThreshold)
+                    # colorStatus = redColorStatus
+
                     d = DebugData()
                     d.addSphere(measured_cop_left[0:3], radius=0.02)
-                    vis.updatePolyData(d.getPolyData(), 'measured cop left', view=self.view, parent='robot state model').setProperty('Color', redColorStatus)
+                    vis.updatePolyData(d.getPolyData(), 'measured cop left', view=self.view, parent='robot state model').setProperty('Color', colorStatus)
                 else:
                     d = DebugData()
                     vis.updatePolyData(d.getPolyData(), 'measured cop left', view=self.view, parent='robot state model')
