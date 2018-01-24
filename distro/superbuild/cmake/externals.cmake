0f8ffdc6c593988917d6543946e47d4eebcdf719
manuelli <lucas.t.manuelli@gmail.com>
2018-01-24 14:33:02 -0500

    bump cv-utils

--- distro/superbuild/cmake/externals.cmake
+++ distro/superbuild/cmake/externals.cmake
@@ -540,7 +540,7 @@ if(USE_PERCEPTION)
 
   ExternalProject_Add(cv-utils
     GIT_REPOSITORY https://github.com/manuelli/cv-utils.git
-    GIT_TAG 26b146fedfe3dcfb47d932911744994b815c4137
+    GIT_TAG 47dc8c74a82563ce757d7d9f405d459d2207710b
     CMAKE_CACHE_ARGS
       ${default_cmake_args}
     DEPENDS
